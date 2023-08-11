04d138a0c97ed937dce83067672a4c0d6a42b16a
Kamilbrdd <kamilbrdek@gmail.com>
2023-08-11 11:38:26 +0200

    add

--- portfolioLabCharity/src/main/java/pl/coderslab/charity/InstitutionRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.coderslab.charity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-    public interface InstitutionRepository extends JpaRepository<Institution, Long> {
-    }
